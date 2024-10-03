539679c197bf5d91940bccf537ac0be531341bc6
ebeshero <ebbondar@gmail.com>
2024-10-02 22:32:19 -0400

    revised JS and CSS for SVG from meeting Wed Oct 2

--- svgScript.js
+++ svgScript.js
@@ -1,35 +1,42 @@
-
-
-
-
 const chain = document.getElementsByClassName('chain');
 const doubleStitch = document.getElementsByClassName('doubleStitch');
 const slipSt = document.getElementsByClassName('slipSt');
-
-chain.addEventListener
-
-
-
-const info = document.getElementById('info');
-document.addEventListener('mousemove', (e) => {
-
-    const height = info.offsetHeight;
-    const width = info.offsetWidth;
-    console.log(width); //sizing of div#info.info
-    console.log(height);
-    // pos= `(${pageX}, ${pageY})`;
+// const bubble = document.getElementById('info')
 
 
 
-    setTimeout(() => {
-        info.style.left = `${e.clientX + width/2}px`;
-        info.style.top = `${e.clientY - height}px`;
-
-    }, 20);
-});
+window.addEventListener('DOMContentLoaded', init, false)
 
 
+function init() {
+    alert('Hey! Page loaded!')
+    for (let i = 0; i < chain.length; i++) {
+        chain[i].addEventListener('mouseover', showBubble, false);
+    }
 
+}
+
+function showBubble() {
+    // alert('show bubble now!')
+    document.getElementById('info').style.display= 'block';
+    document.addEventListener('mousemove', (e) => {
+
+        const height = info.offsetHeight;
+        const width = info.offsetWidth;
+        console.log(width); //sizing of div#info.info
+        console.log(height);
+        // pos= `(${pageX}, ${pageY})`;
+
+        setTimeout(() => {
+            info.style.left = `${e.clientX + width/2}px`;
+            info.style.top = `${e.clientY - height}px`;
+
+        }, 20);
+
+    });
+
+
+}
 
 
 
